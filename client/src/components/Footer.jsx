d3f73da9e7a13fcf059e339e30487ab2fe5b6d33
JonasKr <113203261+jonas-kr@users.noreply.github.com>
2024-09-26 12:51:10 +0100

    Update Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
 
       <div className="border-t pt-5 border-gray-400 flex flex-col sm:grid grid-cols-[2fr_1fr_1fr] gap-14 my-10 mt-28 text-sm">
         <div>
-          <img src={assets.logo4} className="mb-5 w-32 sm:w-36" alt="logo" />
+          <img src={assets.logo} className="mb-5 w-32 sm:w-36" alt="logo" />
           <p className="w-full md:w-2/3 text-gray-600">
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p></div><div><p className="text-xl font-medium mb-5">COMPANY</p>
           <ul className="flex flex-col gap-1 text-gray-600">
